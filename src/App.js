ac7723ad07341d6279e803fdf175c0f67d78205a
lauricewardini <lauricewardini@gmail.com>
2025-02-08 11:12:02 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DozenBoxBuilder from "./components/DozenBoxBuilder";
+
+function App() {
+  return (
+    <div className="App">
+      <DozenBoxBuilder />
+    </div>
+  );
+}
+
+export default App;
